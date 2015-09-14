065a25efb75bbf2b981c1a24f86a68f886346984
Tim Svensson <svensson_tim@hotmail.se>
2015-09-14 14:14:07 +0200

    Added backend.h, sorted what was to be in warehouse.h
    Sorterade ut vad som behövs för front-end och back-end och la dom i två
    olika .h filer. :)

---
+++ backend.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <string.h>
+
+typedef struct warehouse_header warehouse_header_t;
+typedef struct warehouse warehouse_t;
+typedef struct item item_t;
+
+
+void add_item(char *name, char *description, int price,
+	      char *ware_loc, int quantity);
+void remove_item(char *name);
+void edit_item();
+void print_warehouse();
